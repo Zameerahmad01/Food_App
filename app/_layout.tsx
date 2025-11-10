6856a750cc35223b4f98e9edefdcd8684d2e237e
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-11-10 21:08:36 +0500

    Enhance project setup with Tailwind CSS integration, add custom utility classes, and implement new font styles. Introduce image and icon assets, and establish constants for offers and categories. Update Babel configuration and package dependencies for improved functionality.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
-import { Stack } from "expo-router";
-import './global.css'
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, fontError] = useFonts({
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+  });
+  useEffect(() => {
+    if (fontError) {
+      throw fontError;
+    }
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,104 @@
-import { Text, View } from "react-native";
+import { images, offers } from "@/constants";
+import cn from "clsx";
+import { Fragment } from "react";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-bold text-blue-500">
-        Welcome to Nativewind with Expo!
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        data={offers}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+          return (
+            <View>
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row",
+                  index === 0 && "h-56"
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#fffff22" }}
+              >
+                {({ pressed }) => (
+                  <Fragment>
+                    <View
+                      className={cn(
+                        "h-full w-1/2",
+                        index === 0 && "w-2/3 absolute right-0"
+                      )}
+                    >
+                      <Image
+                        source={item.image}
+                        className="size-full"
+                        resizeMode="contain"
+                      />
+                    </View>
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "pl-10" : "pr-10",
+                        index === 0 && "justify-between py-8"
+                      )}
+                    >
+                      <Text
+                        className={cn(
+                          "h1-bold text-white leading-tight",
+                          index === 0 && "max-w-40"
+                        )}
+                      >
+                        {item.title}
+                      </Text>
+                      {index === 0 ? (
+                        <Text className="h1-bold text-white leading-tight ">
+                          $10.00
+                        </Text>
+                      ) : (
+                        <Image
+                          source={images.arrowRight}
+                          className="size-10"
+                          resizeMode="contain"
+                          tintColor="#ffffff"
+                        />
+                      )}
+                    </View>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+        contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => {
+          return (
+            <View className=" flex-between flex-row w-full my-5">
+              <View className="flex-start">
+                <Text className="small-bold text-primary">DELIVER To</Text>
+                <TouchableOpacity className="flex-center flex-row  gap-x-1 mt-0.5">
+                  <Text className="paragraph-bold text-dark-100">Depalpur</Text>
+                  <Image
+                    source={images.arrowDown}
+                    className="size-3"
+                    resizeMode="contain"
+                  />
+                </TouchableOpacity>
+              </View>
+
+              <Text>Cart</Text>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
   );
 }
