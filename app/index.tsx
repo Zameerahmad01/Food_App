d04ccec11435c7da0318c5bd22427e93c75c87cb
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-11-11 20:57:08 +0500

    Implement CartButton component

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import CartButton from "@/components/CartButton";
 import { images, offers } from "@/constants";
 import cn from "clsx";
 import { Fragment } from "react";
@@ -94,7 +95,7 @@ export default function Index() {
                 </TouchableOpacity>
               </View>
 
-              <Text>Cart</Text>
+              <CartButton />
             </View>
           );
         }}
--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,11 +1,18 @@
+import { images } from "@/constants";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 const CartButton = () => {
+  const totalItems = 10;
   return (
-    <View>
-      <Text>CartButton</Text>
-    </View>
+    <TouchableOpacity className="cart-btn" onPress={() => {}}>
+      <Image source={images.bag} className="size-5" resizeMode="contain" />
+      {totalItems > 0 && (
+        <View className="cart-badge">
+          <Text className="small-bold">{totalItems}</Text>
+        </View>
+      )}
+    </TouchableOpacity>
   );
 };
 
